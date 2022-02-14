7093ba03afb3ce0aa6c14ca93e6bbddfa71de731
Alan Yi <yi.alan10@gmail.com>
2022-02-13 22:54:53 -0800

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import Class from '../Class/Class';
+import "./Home.css"
+
+
+function Home(props) {
+
+    const [value, setValue] = useState('');
+    const [favoriteClasses, setClasses] = useState([]);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!favoriteClasses.includes(value)) {
+            setClasses(favoriteClasses.concat(value));
+            setValue('');
+        }
+        console.log(favoriteClasses)
+    }
+
+    document.title = "Classes";
+    return (
+        <div>
+            <h1>Favorite Classes!</h1>
+            <form onSubmit = {handleSubmit}>
+                <label>Add Favorite Class</label>
+                <input type='text' value = {value} onChange={handleChange}></input>
+                <button type='submit'>Add Class!</button>
+            </form>
+            <div className='classes'>
+                {favoriteClasses.map((favClass) =>
+                    <Class name={favClass} key={favClass}></Class> 
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
